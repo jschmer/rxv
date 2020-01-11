153e70449ba4ee4b396e99da1522282ae3cd4fbd
Jens Schmer <jens.schmer+git@gmail.com>
2020-01-11 11:43:06 +0100

    add support for STRAIGHT surround mode

--- tests/live_rxv479_tests.py
+++ tests/live_rxv479_tests.py
@@ -8,7 +8,19 @@ class TestRXV479(testtools.TestCase):
 
     def test_live_action(self):
         rec = rxv.RXV(REAL_IP)
-        rec.server_select([1, 4, 18, 1])
+        #rec.server_select([1, 4, 18, 1])
+        #time.sleep(1)
+        #play_status = rec.play_status()
+        #self.assertEqual("Rock Antenne", play_status.artist)
+
+        print(rec.surround_programs())
+
+        print(rec.surround_program)
         time.sleep(1)
-        play_status = rec.play_status()
-        self.assertEqual("Rock Antenne", play_status.artist)
+        rec.surround_program = "Straight"
+        time.sleep(1)
+        rec.surround_program = "5ch Stereo"
+        time.sleep(1)
+        rec.surround_program = "Straight"
+        time.sleep(1)
+        rec.surround_program = "Surround Decoder"
